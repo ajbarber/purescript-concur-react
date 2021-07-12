80241bec066f4eaf5124c81185dd814f0f229b1c
Adam Barber <barberadam995@gmail.com>
2021-07-12 22:12:48 +0800

    idea for hooking lifecycle events up to neverending widgets

--- src/Concur/React.purs
+++ src/Concur/React.purs
@@ -3,7 +3,7 @@ module Concur.React where
 import Prelude
 
 import Concur.Core.Discharge (discharge, dischargePartialEffect)
-import Concur.Core.Types (Widget)
+import Concur.Core.Types (Widget, display)
 import Data.Either (Either(..))
 import Data.Tuple (Tuple(..))
 import Effect (Effect)
@@ -20,14 +20,15 @@ type ComponentState
 mkComponentState :: HTML -> ComponentState
 mkComponentState v = { view: v }
 
-componentClassWithMount :: forall a. Effect Unit -> Widget HTML a -> R.ReactClass {}
-componentClassWithMount onMount winit = R.component "Concur" component
+componentClassWithMount :: forall a. Effect Unit -> Effect Unit -> Widget HTML a -> R.ReactClass {}
+componentClassWithMount onMount onUnmount winit = R.component "Concur" component
   where
     component this = do
       Tuple winit' v <- dischargePartialEffect winit
       pure { state: mkComponentState v
            , render: render <$> R.getState this
            , componentDidMount: onMount *> handler this (Right winit')
+           , componentWillUnmount: onUnmount
            }
     handler this (Right r) = do
       v <- discharge (handler this) r
@@ -38,7 +39,25 @@ componentClassWithMount onMount winit = R.component "Concur" component
     render st = R.toElement st.view
 
 componentClass :: forall a. Widget HTML a -> R.ReactClass {}
-componentClass = componentClassWithMount mempty
+componentClass = componentClassWithMount mempty mempty
 
 renderComponent :: forall a. Widget HTML a -> R.ReactElement
 renderComponent init = R.createLeafElement (componentClass init) {}
+
+renderComponentWithLifeCycle ::
+  forall a.
+  Effect Unit ->
+  Effect Unit ->
+  Widget HTML a ->
+  R.ReactElement
+renderComponentWithLifeCycle onMount onUnmount w = R.createLeafElement (componentClassWithMount onMount onUnmount w) {}
+
+runAsComponent ::
+  forall a.
+  Effect Unit ->
+  Effect Unit ->
+  Widget HTML a ->
+  Widget HTML a
+runAsComponent onMount onUnmount w = display jsx
+  where
+    jsx = [ renderComponentWithLifeCycle onMount onUnmount w ]
